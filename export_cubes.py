6f9a242aac36536bd05f135d9c444c94c62d2b40
Mahimajain25 <96101074+Mahimajain25@users.noreply.github.com>
2024-12-29 09:13:54 +0530

    Update export_cubes.py

--- export_cubes.py
+++ export_cubes.py
@@ -3,6 +3,11 @@ from utils import write_to_csv
 
 # Function to export cubes with their respective dimensions
 def export_cubes_with_dimensions(tm1):
+    st.markdown("""   
+        <h1 style='text-align: center; font-family: Arial;'>
+            <span style='color: #FFD700;'>Tm1</span><span style='color: #4682B4;'>Exportify</span>
+            </h1>
+            """, unsafe_allow_html=True)
     st.title("Export All Cubes with its Dimensions")
     cubes_name = st.selectbox("List of all Cubes click Export button to download", tm1.cubes.get_all_names())
     
